216c3c6d429762e93e5202673788ae672f55ef73
ManhVDb <manhvdb@bkav.com>
2021-05-11 17:29:16 +0700

    add

---
+++ Html-Css/beudo/Chart.js
@@ -0,0 +1,16 @@
+var LineChart = {
+    labels: ["Ruby", "jQuery", "Java", "ASP.Net", "PHP"],
+    datasets: [{
+        fillColor: "rgba(151,249,190,0.5)",
+        strokeColor: "rgba(255,255,255,1)",
+        pointColor: "rgba(220,220,220,1)",
+        pointStrokeColor: "#fff",
+        data: [10, 20, 30, 40, 50]
+    }, {
+        fillColor: "rgba(252,147,65,0.5)",
+        strokeColor: "rgba(255,255,255,1)",
+        pointColor: "rgba(173,173,173,1)",
+        pointStrokeColor: "#fff",
+        data: [28, 68, 40, 19, 96]
+    }]
+}
